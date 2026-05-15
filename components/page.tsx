2d5afbc2af5aab9e015af8fcbd15e23f6512f3d0
oskarjonsso05-hub <oskarjonsso05@gmail.com>
2026-05-15 17:50:03 +0200

    Update page.tsx

--- components/page.tsx
+++ components/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { Navbar } from '@/components/navbar'
-import { StreamerDashboard } from '@/components/streamer-dashboard'
+import { MyStreams } from '@/components/my-streams'
 
-export default function DashboardPage() {
+export default function MyStreamsPage() {
   return (
     <main className="min-h-screen bg-background">
       <Navbar />
       <div className="pt-20 max-w-7xl mx-auto">
-        <StreamerDashboard />
+        <MyStreams />
       </div>
     </main>
   )
